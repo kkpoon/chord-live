d01eec2c0efd54f3ff6a4f4af36c61a5f47ff53f
K.K. POON <191992+kkpoon@users.noreply.github.com>
2022-03-01 22:10:19 +0000

    Add WebAPI midi access

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+function onMIDIMessage(event) {
+  console.log(event);
+}
+
 (() => {
-  console.log(Tonal.Key.minorKey("Ab"));
+  navigator.requestMIDIAccess().then((midi) => {
+    console.log("MIDI Ready");
+    midi.inputs.forEach((entry) => { entry.onmidimessage = onMIDIMessage; });
+  }, (err) => {
+    console.error(`Failed to get MIDI access - ${err}`);
+  });
+
 })();
+
+console.log(Tonal.Key.minorKey("Ab"));
