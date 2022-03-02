04b28fb1b784c4796cd409d640e9ffc26f137895
K.K. POON <191992+kkpoon@users.noreply.github.com>
2022-03-02 20:19:21 +0000

    display to html

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 const notes = new Set();
 
 function onMIDIMessage(event) {
-  if (event.data.length > 1) {
-    const { data } = event;
+  if (event.data.length === 1) {
+    return
+  }
+  const { data } = event;
+
+  // https://fmslogo.sourceforge.io/manual/midi-table.html
+  if (![128, 144].includes(data[0])) {
+    return;
+  }
+
+  const note = Tonal.Midi.midiToNoteName(data[1], { pitchClass: true });
+
+  if (data[0] === 144) {
+    notes.add(note);
+  } else if (data[0] === 128) {
+    notes.delete(note);
+  }
+
+  const possibleChords = Tonal.Chord.detect(Array.from(notes));
+
+  if (possibleChords.length !== 0) {
+    const [heroChord, ...otherChord] = possibleChords;
     
-    if (data[0] !== 128 && data[0] !== 144) {
-      return;
-    }
+    document.querySelectorAll(".hero-chord").forEach(el => {
+      el.textContent = heroChord;
+    });
 
-    const note = Tonal.Midi.midiToNoteName(data[1], { pitchClass: true });
-
-    if (data[0] === 144) {
-      notes.add(note);
-    } else if (data[0] === 128) {
-      notes.delete(note);
-    }
-
-    console.log(Tonal.Chord.detect(Array.from(notes)));
+    document.querySelectorAll(".other-chord").forEach(el => {
+      el.textContent = otherChord;
+    });
   }
 }
 
